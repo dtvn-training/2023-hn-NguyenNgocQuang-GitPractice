fa5585be151e2dcefec8c1d662488822df47a24d
Ngoc Quang <nguyenngocquang08092002@gmail.com>
2023-11-13 14:29:48 +0700

    Initial commit

---
+++ transaction.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bankaccounts(
+accountno varchar(20) PRIMARY KEY NOT NULL,
+ funds decimal(8,2)
+ );
+ 
+INSERT INTO bankaccounts VALUES("ACC1", 1000);
+INSERT INTO bankaccounts VALUES("ACC2", 2000);
+INSERT INTO bankaccounts VALUES("ACC3", 3000);
+INSERT INTO bankaccounts VALUES("ACC4", 2000);
+
